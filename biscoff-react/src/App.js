ada39be6f50a8fad599a719485dfc06967750ef6
Adeline Graciani <adelinegraciani@Adelines-MacBook-Pro.local>
2020-11-17 13:38:43 +0800

    successfully created test link to bakedgoods url

--- biscoff-react/src/App.js
+++ biscoff-react/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 
 import './App.css';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Bakedgoods from './components/bakedgoods'
 import NavBarItems from './components/NavBarItems'
 
 class App extends React.Component{
@@ -13,17 +15,27 @@ class App extends React.Component{
 
   render() {
     return (
-    <div>
+      <Router>
+        <Switch>
+          <Route path="/bakedgoods">
+            <Bakedgoods />
+          </Route>
+          <Route path = "/">
+          <div>
       <Navbar />
       <div className="homepage-background">
         <div className="homepage-words">
         <h1 className="homepage-words">Artisanal Breads & Cakes Freshly Baked Everyday</h1>
         </div>
-        <button type="button" class="btn btn-light fresh-bakes">See our fresh bakes</button>
-      
+        <Link to="/bakedgoods" className="btn btn-light fresh-bakes">See our fresh bakes</Link>
       </div>
       <Footer />
     </div>  
+          </Route>
+        </Switch>
+       
+      </Router>
+    
       
 
     )
---
+++ biscoff-react/src/components/bakedgoods.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Bakedgoods extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {bakedgoods: ""}
+    }
+
+    render(){
+        if (this.state.bakedgoods ===""){
+            return (
+                <p>test</p>
+            )
+        } else {
+            return (
+                <div>
+                    <p>test that link is created</p>
+                </div>
+            )
+        }
+    }
+}
+
+export default Bakedgoods
